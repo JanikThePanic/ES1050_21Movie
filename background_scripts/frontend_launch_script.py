c813367c67675c581bd67cb4e41b06d403351b0e
Jahangir Abdullayev <jk4abdl@gmail.com>
2023-03-11 00:14:54 -0500

    launch script scans assets folder

--- background_scripts/frontend_launch_script.py
+++ background_scripts/frontend_launch_script.py
@@ -3,7 +3,7 @@ import os
 # dir with all movie files
 # will also make the movies.txt file there
 # absolute location
-dir_path = r"C:\Users\janik\Documents\GitHub\ES1050_21Movie"
+dir_path = r"./assets"
 
 # list to store files
 movie_names = []
@@ -13,14 +13,13 @@ for path in os.listdir(dir_path):
     # check if current path is a file
     if os.path.isfile(os.path.join(dir_path, path)):
         movie_names.append(path)
-print(movie_names)
 
 
 # open file in write mode
 with open(dir_path+"\movies.txt", 'w') as fp:
-	for item in movie_names:
+    for item in movie_names:
         # write each item on a new line
-		fp.write("%s\n" % item)
+        fp.write("%s\n" % item)
 
 
 # LAUNCH index.html IN FULLSCREEN
