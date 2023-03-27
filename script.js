aa9c2d43244bb89ec4da3cda0aae559e1ba10d2e
Evan Cowan <evancowan1@gmail.com>
2023-03-26 20:54:45 -0400

    updated button control system
    added delay to the button controls to prevent double pressing, movie can also be paused or played using the "B" button while movie is playing

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 $('#video').hide();
+// define system
 let system = {
   index: 0,
   total: movies.length-1,
-  play: false
+  play: false,
+  cooldown: false,
+  timeout: undefined
 }
 update();
 
+// key input control
 document.addEventListener('keydown', (event) => {
-  if(event.keyCode == 65) { // A
+  if(event.keyCode == 65 && !system.cooldown) { // A
+    cooldown();
+
+    // start the menu / start the movie
     if(!system.play) {
       startMovie();
     } else {
       startMenu();
     }
-  } else if(event.keyCode == 66) { // B
+  } else if(event.keyCode == 66 && !system.cooldown) { // B
+    cooldown();
+
+    // skip to next movie / pause or play
     if(!system.play) {
       advance();
+    } else {
+      if($("#video").get(0).paused) {
+        $('#video').get(0).play();
+      } else {
+        $('#video').get(0).pause();
+      } 
     }
   }
 }, false);
 
+// button press cooldown
+function cooldown() {
+  system.cooldown = true;
+  clearTimeout(system.timeout);
+  system.timeout = setTimeout(() => {
+    cooldownReset();
+  }, "250");
+}
+function cooldownReset() {
+  system.cooldown = false;
+}
+
+// start menu when video ends
 $(document).ready(function(){
   $('#video').on('ended',function(){
     startMenu();
   });
 });
 
+// menu controls
 function startMenu() {
   system.play = false;
   $('.gallery').show();
@@ -34,6 +64,7 @@ function startMenu() {
   $('#video').get(0).pause();
 }
 
+// movie controls
 function startMovie() {
   let path = drive_path + 'movies/' + movies[system.index].title + '.' + movies[system.index].type;
   system.play = true;
@@ -45,6 +76,7 @@ function startMovie() {
   $('#video').get(0).play();
 }
 
+// advance the movie index
 function advance() {
   if(system.index != system.total) {
     system.index++;
@@ -55,6 +87,7 @@ function advance() {
   console.log("Advanced to movie: "+ movies[system.index].title + " with index: " + system.index)
 }
 
+// calculate next index
 function rollover(i, n) {
   if (i > n) {
       return 0;
@@ -65,6 +98,7 @@ function rollover(i, n) {
   }
 }
 
+// update gallery images
 function update() {
   let left = rollover(system.index+1, system.total);
   if(movies[left].thumbnail) {
