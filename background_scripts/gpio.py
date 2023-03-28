5531860909d5ae549c1c60d2d7600d4e6cdb44b2
Janik <48893696+JanikThePanic@users.noreply.github.com>
2023-03-28 02:37:36 -0400

    prepared gpio.py script

---
+++ background_scripts/gpio.py
@@ -0,0 +1,9 @@
+# for amit
+# button wires need to go 3.3v to a gpio
+# needed links
+# https://libre.computer/images/posts/mmexport1510488638659.webp
+# https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
+# https://docs.google.com/spreadsheets/d/1ruDmSzQexpXX2iCCqma7Vwu6t_ZlEt8robvI0qSdbwk/edit#gid=0
+
+# just make a loop and map left key to "a", and right to "b" (lowercase)
+# ill add this .py to the auto launch once youre done
